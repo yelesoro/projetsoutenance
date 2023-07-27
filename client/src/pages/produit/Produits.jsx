d7ddd451cc69a314c38a6130d441ec0bdfaab29a
yelesoro <soroyele2020@gmail.com>
2023-07-27 09:07:17 +0100

    4eme commit

--- client/src/pages/produit/Produits.jsx
+++ client/src/pages/produit/Produits.jsx
@@ -5,14 +5,30 @@ import Header from "./header/Header";
  '../../ImageProduits/ananas.jpg'
  import { Link } from "react-router-dom";
 import ananas from '../../ImageProduits/caco.png'
-import tomate from '../../ImageProduits/tomatediarra.png'
+// import tomate from '../../ImageProduits/tomatediarra.png'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import './produits.scss'
 
 
 const Produits = () => {
 
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const apiUrl = 'http://localhost:3000/api/get';
     
+        // Appel à l'API avec Axios
+        axios.get(apiUrl)
+          .then(response => {
+            setData(response.data);
+          })
+          .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+          });
+      }, []);
+
     
     return (
         
@@ -24,23 +40,19 @@ const Produits = () => {
              <center> <div className="tete"><h1 className="heading">NOS <span>PRODUITS</span></h1></div></center><br /><br />
 
                 <div className="box-container">
-                    <div className="box">
+                    {data.map(item=>(
+                        <div className="box" key={item.id}>
                         <img src={ananas} alt="image anans" />
                         <div className="content">
-                            <h3>Délicieux plats</h3>
+                            <h3>{item.nomEtud}</h3>
                             <p>Lorem ipsum dolor sit amet consectetur </p><br /><br />
                             <Link to={'/vendeurs'}><span className="btna">Acheter</span></Link>
                         </div>
-                    </div><br />
+                    </div>
+                    ))}
+                    <br />
 
-                    <div className="box">
-                        <img src={tomate} alt="image anans" />
-                        <div className="content">
-                            <h3>Délicieux plats</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur </p><br />
-                            <Link to={'/vendeurs'}><span className="btna">Acheter</span></Link>
-                        </div>
-                    </div><br />
+                    
 
                 </div>
 
